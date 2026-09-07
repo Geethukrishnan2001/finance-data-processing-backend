966b880747147372d9fc20eecf62c4d094a3f465
geethu <geethu8366@gmail.com>
2026-09-07 15:20:59 +0530

    Update permissions

--- api/permissions.py
+++ api/permissions.py
@@ -17,13 +17,14 @@ class IsViewer(BasePermission):
 
 class RecordPermission(BasePermission):
     def has_permission(self, request, view):
+
+        if not request.user or not request.user.is_authenticated:
+            return False
+
         if request.user.role == 'admin':
             return True
-        
-        if request.user.role == 'analyst':
+
+        if request.user.role in ['analyst', 'viewer']:
             return request.method in SAFE_METHODS
-        
-        if request.user.role == 'viewer':
-            return request.method in SAFE_METHODS
-        
-        return False
+
+        return False 
